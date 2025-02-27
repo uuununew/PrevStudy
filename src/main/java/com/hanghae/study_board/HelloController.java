3671aa7aea5714244f4869ebf3ac5e0b1700f44c
yujin <uuununew@gmail.com>
2025-02-27 16:14:54 +0900

    create project

---
+++ src/main/java/com/hanghae/study_board/HelloController.java
@@ -0,0 +1,18 @@
+package com.hanghae.study_board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello");
+        return "hello";
+    }
+}
